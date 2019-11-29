94aea567c9d78aed750335f0002411c281102c41
StiflerPasha <dvoynospavel@gmail.com>
2019-11-29 14:27:21 +0300

    make it better

--- handlers/VPNKeysHandler.js
+++ handlers/VPNKeysHandler.js
@@ -3,27 +3,27 @@ import { getPageContent } from '../helpers/puppeteer';
 
 const SITE = `https://vk.com/hidemy_name_keys`;
 
-export default async () => {
+export const getVPNKeys = async () => {
   try {
     const pageContent = await getPageContent(SITE);
     const $ = cheerio.load(pageContent);
     const firstPost = $('#page_wall_posts div:first-of-type');
     
     const date = firstPost
-     .find('.post_date')
-     .first()
-     .text();
+      .find('.post_date')
+      .first()
+      .text();
     
     const keys = firstPost
-     .find('.wall_post_text')
-     .first()
-     .text()
-     .match(/\d{14}/g);
+      .find('.wall_post_text')
+      .first()
+      .text()
+      .match(/\d{14}/g);
     
     console.log(date);
     console.log(keys);
     
   } catch (err) {
-    throw err
+    throw err;
   }
 };
